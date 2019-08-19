b6ff7fbb625a2cb5784a1056649a537ddf3a6a43
KimberlySwinton <kimberlyswinton@gmail.com>
2019-08-19 10:41:42 -0400

    MVP

---
+++ safezoos/src/main/java/com/lambdaschool/zoos/view/CountAnimalsInZoo.java
@@ -0,0 +1,8 @@
+package com.lambdaschool.zoos.view;
+
+public interface CountAnimalsInZoo {
+    // db is going to return the animal name and the zoo count
+    String getAnimalname();
+
+    int getZoocount();
+}
